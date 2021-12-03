b7a646d542c5f77e2a2878c69a7abb5c95630e8f
Fabio FNC <fabiohenrique.7670br@gmail.com>
2021-12-03 17:35:04 -0300

    🧱 Base do projeto.

---
+++ src/main/java/br/com/fabiofnc/apileilao/ApiLeilaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.fabiofnc.apileilao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiLeilaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiLeilaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fabiofnc/apileilao/ApiLeilaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fabiofnc.apileilao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiLeilaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
